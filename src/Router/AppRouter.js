7e6af99e486d17c487f6ae3af53866f5db49d08f
dohaianh <dha190320022@gmail.com>
2022-10-20 07:43:41 +0700

    update app

--- src/Router/AppRouter.js
+++ src/Router/AppRouter.js
@@ -8,7 +8,7 @@ import AdminPage from "../Components/AdminPage/AdminPage.js";
 
 export default function AppRouter() {
   return (
-    <Router basename="/blog">
+    <Router basename="/Blog">
       <Routes>
         <Route path="/" element={<BlogContainer />} />
         <Route path="/login" element={<Login />} />
