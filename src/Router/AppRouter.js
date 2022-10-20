b27d8662b92409f0e8eb0c09b06c1ad6f9307329
dohaianh <dha190320022@gmail.com>
2022-10-20 07:12:56 +0700

    change router

--- src/Router/AppRouter.js
+++ src/Router/AppRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "../Components/Login/Login.js";
 import BlogContainer from "../Components/BlogContainer/BlogContainer.js";
